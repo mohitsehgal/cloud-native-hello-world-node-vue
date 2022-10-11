96567e17488ed9fe356ff1c5f6fa172816efec45
Mohit Sehgal <mohit@relifems.com>
2022-10-11 12:29:44 +0530

    api call

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup>
 import TheWelcome from "../components/TheWelcome.vue";
+import { ref } from 'vue'
+const data = ref({})
+const error = ref({})
+
+fetch('http://localhost:3000/api/users')
+  .then((res) => res.json())
+  .then((json) => (data.value = json))
+  .catch((err) => (error.value = err))
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>User Name: {{data[0].name}}</h1>
   </main>
 </template>
